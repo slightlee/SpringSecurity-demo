d0a2230cfe2927d74ac610395da1f3395d551478
morrow <limingwei@mentpeak.com>
2021-08-23 18:09:06 +0800

    feat: 2、基于内存密码模式添加刷新令牌

--- authorization-server/src/main/java/com/morrow/config/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/com/morrow/config/AuthorizationServerConfig.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 
 /**
   * 认证服务配置
@@ -21,6 +24,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private final AuthenticationManager authenticationManager;
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -39,13 +44,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                  //Client 账号、密码
                  .withClient("clientapp").secret("111222")
                 // 密码模式
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password","refresh_token")
                 // 可授权的 Scope
-                .scopes("all");
+                .scopes("all")
+                 // 访问令牌的有效期为 3600 秒 = 2 小时
+                 .accessTokenValiditySeconds(3600)
+                 // 刷新令牌的有效期为 864000 秒 = 10 天
+                 .refreshTokenValiditySeconds(864000)
+         ;
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager)
+             .userDetailsService(userDetailsService)
+        ;
     }
+
 }
